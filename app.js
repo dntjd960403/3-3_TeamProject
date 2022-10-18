2e9dd90ccd9f3ff12d828add5c4bf6124c9d09dd
dntjd960403 <dntjd960403@gmail.com>
2022-10-18 17:55:27 +0900

    swagger 스웩

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const Http = require('http');
 const routes = require('./routes');
+var cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
 require('dotenv').config();
 
 const cookieParser = require('cookie-parser');
-
 const app = express();
 const http = Http.createServer(app);
-const port = process.env.EXPRESS_PORT || 3000;
+const port = process.env.EXPRESS_PORT || 3030;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/', routes);
+app.use(cors());
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 http.listen(port, () => {
   console.log(`Start listen Server: ${port}`);
