73f522ada68ead964f79b4357dde8b0be46a9e1c
KJJDSA <kjjdsa@gmail.com>
2022-10-18 18:47:18 +0900

    하나 안했음

--- services/posts.js
+++ services/posts.js
@@ -4,11 +4,11 @@ const PostsRepository = require('../repositories/posts');
 
 class PostsService {
 
-  postRepository = new PostsRepository();
+  postsRepository = new PostsRepository();
 
   findAllPost = async () => {
     // 찾기 
-    const allPost = await this.postRepository.findAllPost();
+    const allPost = await this.postsRepository.findAllPost();
     // 정렬하기
     allPost.sort((a, b) => {
       return b.createdAt - a.createdAt;
@@ -20,7 +20,7 @@ class PostsService {
 
   createPost = async (userId, title, content) => {
     // 만들기
-    const createPostData = await this.postRepository.createPost(
+    const createPostData = await this.postsRepository.createPost(
       userId,
       title,
       content
@@ -32,7 +32,7 @@ class PostsService {
 
   findPostById = async (postId) => {
     // 상세정보 찾기
-    const findPost = await this.postRepository.findPostById(postId);
+    const findPost = await this.postsRepository.findPostById(postId);
 
     // 찾은 정보 올리기
     return findPost;
@@ -40,23 +40,23 @@ class PostsService {
 
   updatePost = async (userId, postId, title, content) => {
     // 업데이트할 게시글 찾기
-    const findPost = await this.postRepository.findPostById(postId);
+    const findPost = await this.postsRepository.findPostById(postId);
     if (!findPost) return "삭제된 게시물입니다.";
     if (userId !== findPost.userId) return "수정할 수 없는 게시물입니다.";
     // 수정하기 
-    await this.postRepository.updatePost(userId, postId, title, content);
+    await this.postsRepository.updatePost(userId, postId, title, content);
 
-    const updatePost = await this.postRepository.findPostById(postId);
+    const updatePost = await this.postsRepository.findPostById(postId);
     return updatePost;
   };
 
   deletePost = async (userId, postId) => {
     // 삭제하기 전 게시글 찾아보기
-    const findPost = await this.postRepository.findPostById(postId);
+    const findPost = await this.postsRepository.findPostById(postId);
     if (!findPost) return ("삭제된 게시물입니다.");
     if (userId !== findPost.userId) return "삭제할 수 없는 게시물입니다."
     // 삭제하기
-    await this.postRepository.deletePost(userId, postId);
+    await this.postsRepository.deletePost(userId, postId);
     return findPost;
   };
 }
