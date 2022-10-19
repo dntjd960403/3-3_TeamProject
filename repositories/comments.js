db3234be165f14574d617be7667ef17fe2995845
nowhereim <zyle1104@gmail.com>
2022-10-20 05:27:34 +0900

    코멘트 수정

--- repositories/comments.js
+++ repositories/comments.js
@@ -2,67 +2,57 @@ const { Comments } = require('../models');
 const { Sequelize } = require('../models');
 const { Op } = Sequelize;
 
-class Commentsrepository{
-    createComment =  async(postId,comment,userId) => {
-      try{
-        let createcomment = await Comments.create({ postId, userId, comment });
-        return createcomment
-      }catch(error){
-        return error.name +"="+error.errorMessage
-      }
-        
+class Commentsrepository {
+  createComment = async (postId, comment, userId) => {
+    try {
+      let createcomment = await Comments.create({ postId, userId, comment });
+      return createcomment;
+    } catch (error) {
+      return error.name + '=' + error.errorMessage;
     }
+  };
 
-    Commentlist = async(postId) =>{
-      try{
-        const comments = await Comments.findAll({
-          where: { postId },
-          });
-          return comments
-      }catch(error){
-        error.name +"="+error.errorMessage
-      }
-        
-
+  Commentlist = async (postId) => {
+    try {
+      const comments = await Comments.findAll({
+        where: { postId },
+      });
+      return comments;
+    } catch (error) {
+      error.name + '=' + error.errorMessage;
     }
+  };
 
-    Commentedit = async(commentId,comment,userId) => {
-      try{
-        const updateCount = await Comments.update(
-            { comment },
-            { where: { commentId, userId } }
-          );
+  Commentedit = async (commentId, comment, userId) => {
+    try {
+      const updateCount = await Comments.update({ comment }, { where: { commentId, userId } });
 
-          return updateCount
-      }catch(error){
-        return error.name +"="+error.errorMessage
-      }
-        
+      return updateCount;
+    } catch (error) {
+      return error.name + '=' + error.errorMessage;
     }
+  };
 
-    CommentisExist = async(commentId) => {
-      try{
-        const isExist = await Comments.findByPk(commentId);
-      
-        return isExist;
-      }catch(error){
-        return error.name +"="+error.errorMessage
-      }
-        
+  CommentisExist = async (commentId) => {
+    try {
+      const isExist = await Comments.findByPk(commentId);
+
+      return isExist;
+    } catch (error) {
+      return error.name + '=' + error.errorMessage;
     }
-    Commentdelete = async(commentId,userId) => {
-      try{
-        const deleteCount = await Comments.destroy({
+  };
+  Commentdelete = async (commentId, userId) => {
+    try {
+      const deleteCount = await Comments.destroy({
         where: { commentId, userId },
       });
 
       return deleteCount;
-      }catch(error){
-        return error.name +"="+error.errorMessage
-      }
-      
+    } catch (error) {
+      return error.name + '=' + error.errorMessage;
     }
-
+  };
 }
 
-module.exports = Commentsrepository
+module.exports = Commentsrepository;
--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
-Commentscontroller = require("../controllers/comments")
+Commentscontroller = require('../controllers/comments');
 commentscontroller = new Commentscontroller();
 
-router.post("/:postId", authMiddleware, commentscontroller.createComment)
-router.get("/:postId", authMiddleware, commentscontroller.Commentlist)
-router.put("/:commentId", authMiddleware, commentscontroller.Commentedit)
-router.delete("/:commentId", authMiddleware, commentscontroller.Commentdelete)
+router.post('/:postId', authMiddleware, commentscontroller.createComment);
+router.get('/:postId', authMiddleware, commentscontroller.Commentlist);
+router.put('/:commentId', authMiddleware, commentscontroller.Commentedit);
+router.delete('/:commentId', authMiddleware, commentscontroller.Commentdelete);
 
 module.exports = router;
